fc92506f7c09ec6aa8a88a5158da922a0b0ab39a
Sudhansh Kumar <sudhanshkb04@gmail.com>
2024-09-25 22:32:52 +0530

    first commit

---
+++ product.js
@@ -0,0 +1,16 @@
+function change1(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "https://m.media-amazon.com/images/I/61CGHv6kmWL._AC_SX425_.jpg";
+}
+function change2(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "https://m.media-amazon.com/images/I/71z2WmHMtZL._AC_SX425_.jpg";
+}
+function change3(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "https://m.media-amazon.com/images/I/71rxKLsCxqL._AC_SX425_.jpg";
+}
+function change4(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "https://m.media-amazon.com/images/I/71dybzbdO9L._AC_SX425_.jpg";
+}
---
+++ script.js
@@ -0,0 +1,4 @@
+function run(){
+    let selectphoto = document.querySelector('#photo');
+    selectphoto.src = "https://media.istockphoto.com/id/2166605519/photo/a-car-stands-still-on-the-roadside-in-dense-morning-fog.webp?a=1&s=612x612&w=0&k=20&c=WInSXxrhY11ZSp5EtDpc0lw5JoBZ17dxsLGqjPfO5Ss=";
+}
